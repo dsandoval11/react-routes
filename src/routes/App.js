a6bce471b3b52a9c65188c98a35e032f38796c3a
David Sandoval <david281199@gmail.com>
2020-01-15 12:59:41 -0500

    first commit

---
+++ src/routes/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Home from '../containers/Home'
+import Login from '../containers/Login'
+import Register from '../containers/Register'
+import NotFound from '../containers/NotFound'
+
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Home}></Route>
+      <Route exact path="/login" component={Login}></Route>
+      <Route exact path="/register" component={Register}></Route>
+      <Route component={NotFound}></Route>
+    </Switch>
+  </BrowserRouter>
+)
+
+export default App
